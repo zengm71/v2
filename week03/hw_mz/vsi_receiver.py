a3751e912615a076c978ceed687de5812c066a96
michael.zeng <zengm71@ischool.berkeley.edu>
2020-01-19 02:06:16 -0800

    fixed typo in receiver.py

--- week03/hw_mz/vsi_receiver.py
+++ week03/hw_mz/vsi_receiver.py
@@ -9,7 +9,7 @@ def on_connect_local(client, userdata, flags, rc):
 	
 def on_message_local(client, userdata, msg):
   try:
-    print("message received locally!" + msg)	
+    print("message received locally!" + msg.payload)	
     # if we wanted to re-publish this message, something like this should work
     # msg = msg.payload
     # remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
